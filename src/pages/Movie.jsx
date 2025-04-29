29a95fb726a72c3ca474a807744d88813ce6055f
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-04-30 00:06:40 +0530

    Initial Commit

---
+++ src/pages/Movie.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import Card from '../components/UI/Card';
+
+const Movie = () => {
+    const[data,setData]=useState([])
+    const API = "http://www.omdbapi.com/?i=tt3896198&apikey=4c59a226&s=titanic&page=1";
+    const getMovieData = async () => {
+        try {
+            const res = await axios.get(API);
+            console.log(res.data.Search);
+            setData(res.data.Search);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getMovieData();
+    }, []);
+    return <ul>
+        {
+            data.map((curElem)=>{
+                return <Card key={curElem.imdbID} movieData={curElem}/>
+            })
+        }
+    </ul>
+}
+
+export default Movie
